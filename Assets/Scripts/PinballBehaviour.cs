62a2f75c9da8efe456e7feafa7fd5d41959d63b2
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-15 20:59:21 -0600

    Update PinballBehaviour.cs

--- Assets/Scripts/PinballBehaviour.cs
+++ Assets/Scripts/PinballBehaviour.cs
@@ -20,7 +20,7 @@ public class PinballBehaviour : MonoBehaviour
     {
         
     }
-    public void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("DeathZone"))
         {
