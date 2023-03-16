e992d164904f23575a8a74c6505951772a8e2228
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-15 20:44:19 -0600

    eli

--- Assets/Scripts/PinballBehaviour.cs
+++ Assets/Scripts/PinballBehaviour.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PinballBehaviour : MonoBehaviour
 {
+    [SerializeField] GameObject Pinball;
     public int RoundsLeft;
+    private bool shadowRealmed;
 
     // Start is called before the first frame update
     void Start()
     {
         RoundsLeft = 3;
+        shadowRealmed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Respawn();
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    void Respawn()
     {
-        
+        if (shadowRealmed && RoundsLeft != 0)
+        {
+            Instantiate(Pinball, new Vector3(0f, 5f, 0f), Quaternion.identity);
+            shadowRealmed = false;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DeathZone"))
+        {
+            Destroy(this.gameObject);
+            RoundsLeft--;
+            shadowRealmed = true;
+        }
     }
 }
