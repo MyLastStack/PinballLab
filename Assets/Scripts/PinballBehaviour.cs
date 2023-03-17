3c2eb0f6b473db15e2675e268bfff161860fb4a7
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-16 20:02:57 -0600

    updated

--- Assets/Scripts/PinballBehaviour.cs
+++ Assets/Scripts/PinballBehaviour.cs
@@ -8,12 +8,13 @@ public class PinballBehaviour : MonoBehaviour
     Rigidbody2D rb;
     float maxSpeed = 10f;
     public int RoundsLeft;
-    public Vector3 respawnCheck = new Vector3(2.5f, -1f, 0);
+    public Vector3 respawnCheck;
 
     // Start is called before the first frame update
     void Start()
     {
         RoundsLeft = 3;
+        respawnCheck = new Vector3(2.5f, -1f, 0f);
     }
 
     // Update is called once per frame
@@ -41,8 +42,6 @@ public class PinballBehaviour : MonoBehaviour
                 Destroy(this.gameObject);
             }
             Pinball.transform.position = respawnCheck;
-            rb.velocity = Vector2.zero;
-            rb.rotation = 0;
         }
     }
 }
