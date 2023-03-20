7336b7900c9ef6212904a4f138b173b952e7c719
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-19 19:42:40 -0600

    updated

--- Assets/Scripts/PaddleBehaviour.cs
+++ Assets/Scripts/PaddleBehaviour.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PaddleBehaviour : MonoBehaviour
 {
     [SerializeField] HingeJoint2D hinge;
-
-    // Update is called once per frame
-    void Update()
-    {
-        Flip(Keyboard.current.spaceKey.isPressed);
-    }
-
+    //private void Update()
+    //{
+    //    Flip(Keyboard.current.spaceKey.isPressed);
+    //}
     public void Flip(bool isPressed)
     {
         hinge.useMotor = isPressed;
